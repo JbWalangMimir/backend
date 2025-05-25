70bf3d3a059fa95dea14733ab9f7f7c4fdc54271
JbWalangMimir <jbbontogon1@gmail.com>
2025-05-25 09:57:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="orkidAsIyey Image Classifier")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["https://orkidasiyey.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
